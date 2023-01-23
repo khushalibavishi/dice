141b46a4308dedc5eea9b6126078aba9b623134d
khushalibavishi <121333266+khushalibavishi@users.noreply.github.com>
2023-01-23 12:21:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 2;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice"+randomNumber2+".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource2 = "images/" + randomDiceImage;
 
 var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src", randomImageSource);
+image2.setAttribute("src", randomImageSource2);
 
-if(randomNumber1>randomNumber2){
+if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML="🚩player 1 wins!";
 }
-else if(randomNumber2>randomNumber1){
+else if(randomNumber2 > randomNumber1){
     document.querySelector("h1").innerHTML="player 2 wins!🚩";
 }
 else{
